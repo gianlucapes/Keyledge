5aaa8bce4de2e368caa1b6282cd4271b9fea442e
theSevenHack <70605521+theSevenHack@users.noreply.github.com>
2020-09-03 22:05:42 +0200

    Add files via upload

---
+++ source/Password/Password/TextWarning/MessageText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password.TextWarning
+{
+    class MessageText
+    {
+        protected string[] TitleMessage = { "Warning","Error" };
+        protected string[] WarningMessage = { "The system has detected an error restart program", "Set mode" ,"Illegal value","Select text to copy","Minimum value 1", "Value it must be between 1-100" };
+    }
+}

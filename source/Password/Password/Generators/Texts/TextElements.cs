18749f1a5874ace7b7a70bcb65049ff4b6b17cd1
theSevenHack <70605521+theSevenHack@users.noreply.github.com>
2020-09-03 22:21:47 +0200

    Add files via upload

---
+++ source/Password/Password/Generators/Texts/TextElements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password.Makers
+{
+    class TextElements
+    {
+
+        protected Random extractor = new Random();
+        protected string specialCharacters = "!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        protected string letters = "abcdefghijklmnopqrstuvwxyz";
+        protected string upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        protected string digits = "0123456789";
+        protected string randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        protected string prodKeyChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        protected string slash = "-";
+
+    }
+}
